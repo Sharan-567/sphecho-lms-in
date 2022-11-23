a9026c65d24ca18356bb36db87cc0b24190fee71
santhoshm16 <santhosh@m16labs.com>
2022-11-23 19:53:26 +0000

    fix(landingPage): landing pages sinitialized

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,300 @@
-import React from 'react'
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Carousel from "react-bootstrap/Carousel";
+import landing from "../../assets/landing.png";
+import career from "../../assets/career.png";
+import discover from "../../assets/discover.png";
+import enhance from "../../assets/enhance.png";
+import improve from "../../assets/improve.png";
+import noImage from "../../assets/no-img.png";
+import "./home.scss";
+
+// const Home = () => {
+//   return <iframe style={{width: '100vw', height: "100vh"}} src="HTML/index.html"></iframe>
+// }
 
 const Home = () => {
-  return <iframe style={{width: '100vw', height: "100vh"}} src="HTML/index.html"></iframe>
-} 
+  return (
+    <div>
+      <Banner />
+      <TitleContainer />
+      <BenfitsOfCertification />
+      <Board />
+      <Committe />
+      <InternalCommitee />
+    </div>
+  );
+};
 
-export default Home
+const Banner = () => {
+  return (
+    <div
+      className="banner-container"
+      style={{ height: "130vh", width: "100wh" }}
+    >
+      <div className="clip-container">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block coursel-img"
+              src={landing}
+              alt="First slide"
+            />
+            {/* <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption> */}
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+const TitleContainer = () => {
+  return (
+    <div className="title-content">
+      <p>
+        The SPHECHO curriculum and cerification program is for professionals who
+        are responsible for the support and/or services provided to people of
+        determination. The program's objective is to offer a comprehensive,
+        multidisciplinary training program and specialized credential to
+        professionals in order to supplement their existing knowledge, make sure
+        they are up to date on best practices and research, and ensure they
+        incorporate new knowledge into professional practice.
+      </p>
+    </div>
+  );
+};
+
+const BenfitsOfCertification = () => {
+  const benifits = [
+    {
+      id: 1,
+      title: "ENHANCE OUTCOMES",
+      icon: enhance,
+    },
+    {
+      id: 2,
+      title: "DISCOVER NEW STRATEGIES ",
+      icon: discover,
+    },
+    {
+      id: 3,
+      title: `IMPROVE STANDING & CREDIBILITY`,
+      icon: improve,
+    },
+    {
+      id: 4,
+      title: `CAREER DEVELOPMENT PROSPECTS`,
+      icon: career,
+    },
+  ];
+
+  return (
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#F1F0EE"
+          fill-opacity="1"
+          d="M0,96L80,112C160,128,320,160,480,149.3C640,139,800,85,960,74.7C1120,64,1280,96,1360,112L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+        ></path>
+      </svg>
+      <div
+        className="m-auto"
+        style={{ background: "#F1F0EE", marginTop: "-10%", zIndex: 19 }}
+      >
+        <h2 className="benefits-title bg-600 text-center mb-4">
+          BENEFITS OF CERTIFICATION
+        </h2>
+        <Row className="mt-4 w-75 m-auto">
+          {benifits.map((benfit) => (
+            <Col key={benfit.id} sm="3" className="p-2 px-4 mb-3">
+              <div
+                className="bg-landingPrimary br-2 p-2 text-center d-flex flex-column align-items-center justify-content-center"
+                style={{ height: "12rem" }}
+              >
+                <img src={benfit.icon} style={{ width: "4rem" }} />
+                <p className="text-center text-white b-600 mt-3">
+                  {benfit.title}
+                </p>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </div>
+      <svg
+        style={{ marginTop: "-1rem", zIndex: "-400" }}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+      >
+        <path
+          fill="#F1F0EE"
+          fill-opacity="1"
+          d="M0,96L80,122.7C160,149,320,203,480,192C640,181,800,107,960,96C1120,85,1280,139,1360,165.3L1440,192L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+        ></path>
+      </svg>
+    </div>
+  );
+};
+
+const Board = () => {
+  return (
+    <div className="text-center w-75 m-auto">
+      <h1 className="text-landingPrimary b-600 mb-4">SPHECHO BOARD</h1>
+      <p className="my-4" style={{ fontSize: "1.2rem" }}>
+        The SPHECHO certification procedures and content undergo various reviews
+        and validation, pursuant to the professions that are eligible for
+        certification, as well as standards established by content experts in
+        the field across the globe. Please click on a topic below to view common
+        questions. Once you have chosen your topic simply click any question to
+        reveal the answer. Can’t find an answer to your question? Contact us
+        today. Our board at SPHECHO is commitied to ensuring the creation and
+        implementation of world-class training and certification programs that
+        will enhance the general standard of care and instruction for
+        professionals, parents, and caregivers. The Board is made up of a
+        variety of the world's top experts, researchers from prestigious
+        universities, special education directors, corporate leaders, clinicians
+        in the field of mental health, speech therapists, occupational
+        therapists, physiotherapists, behavioral therapists and psychologists,
+        counsellors, audiologists, and parents.
+      </p>
+    </div>
+  );
+};
+
+const Member = ({
+  name,
+  title,
+  img,
+}: {
+  name: string;
+  title: string;
+  img?: string;
+}) => {
+  return (
+    <div>
+      <img
+        className="fluid shadow mb-3"
+        src={noImage}
+        width="180rem"
+        style={{ borderRadius: "50%" }}
+      />
+      <p className="mb-1 b-600" style={{ fontSize: "1.4rem" }}>
+        {name}
+      </p>
+      <p>
+        <i style={{ fontSize: "1.2rem" }}>{title}</i>
+      </p>
+    </div>
+  );
+};
+
+const Committe = () => {
+  const advaisoryMemebers = [
+    {
+      id: 1,
+      name: "Dr. Swaleha Mohamedali",
+      title: "Educational consultant",
+    },
+    {
+      id: 2,
+      name: "Dr. Aamir Khamis Al Toubi",
+      title: "Consultant Speech Language Pathologist",
+    },
+    {
+      id: 3,
+      name: "Udaya Kiran Guttarlapalle",
+      title: "Senior Speech Language Pathologist",
+    },
+  ];
+
+  return (
+    <div className="container" style={{ marginTop: "7rem" }}>
+      <div
+        style={{ width: "90%" }}
+        className="m-auto br-4 bg-landingGary text-center py-5 px-4"
+      >
+        <h5 className="mb-4 pb-3">ADVISORY PANEL MEMBERS</h5>
+        <Row className="mb-2">
+          {advaisoryMemebers.map((member) => (
+            <Col key={member.id} sm="4" className="text-center">
+              <Member {...{ ...member }} />
+            </Col>
+          ))}
+        </Row>
+      </div>
+    </div>
+  );
+};
+
+const InternalCommitee = () => {
+  const members = [
+    [
+      {
+        id: 1,
+        name: "Sathish Chandra Segar",
+        title: `Clinical Director at the Qatar Institute for Speech & Hearing`,
+      },
+      {
+        id: 2,
+        name: "Hebatulla Ibrahim Farrag",
+        title: `Clinical Director and Speech and Language Pathologist at the Qatar Institute for Speech & Hearing`,
+      },
+      {
+        id: 3,
+        name: "Varun G Rai",
+        title: `Clinical Audiologist and Application
+Specialist at the Qatar Institute for
+Speech & Hearing`,
+      },
+    ],
+    [
+      {
+        id: 1,
+        name: "Jaseena NK",
+        title: `Human Resource Manager at
+Qatar Institute for Speech & Hearing`,
+      },
+      {
+        id: 2,
+        name: "Mahmoud El Abassiry",
+        title: `Manager at Qatar Institute for
+Speech & Hearing`,
+      },
+      {
+        id: 3,
+        name: "Tamer Rashed",
+        title: `Manager at Qatar Institute for
+Speech & Hearing Rehabilitation Center`,
+      },
+    ],
+  ];
+
+  return (
+    <div className="container" style={{ marginTop: "7rem" }}>
+      <div
+        style={{ width: "90%" }}
+        className="m-auto br-4 bg-landingGary text-center py-5 px-4"
+      >
+        <h5 className="mb-4 pb-3">INTERNAL REVIEW COMMITTEE</h5>
+        <Row className="mb-4">
+          {members[0].map((member) => (
+            <Col key={member.id} sm="4" className="text-center">
+              <Member {...{ ...member }} />
+            </Col>
+          ))}
+        </Row>
+        <Row className="mb-2">
+          {members[1].map((member) => (
+            <Col key={member.id} sm="4" className="text-center">
+              <Member {...{ ...member }} />
+            </Col>
+          ))}
+        </Row>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
