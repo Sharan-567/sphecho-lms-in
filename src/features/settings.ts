9d9234418c71db9cd0c440d629c1c54ad8250c06
Sharansrj567 <sharansrj567@gmail.com>
2023-05-02 15:32:58 +0530

    fix(*): host-url and hardcoded fix

--- src/features/settings.ts
+++ src/features/settings.ts
@@ -2,9 +2,9 @@ export const HOST = "learn-api.sphecho.tz";
 export const LOCAL = "localhost:8000";
 // change this on production
 export const BASE_URL = `https://${HOST}/lms_api_v1`;
-export const QISH_URL = "https://qish.metahos.com";
+export const QISH_URL = "https://service.sphecho.com";
 export const SHOP_URL = "https://qishcommerce.metahos.com";
-export const SCHOLAR_URL = "https://qish.metahos.com/scholar";
+export const SCHOLAR_URL = "https://service.sphecho.com/scholar";
 
 export const SERVICE_URLS = {
   speech: `${QISH_URL}/service/departments/speech`,
